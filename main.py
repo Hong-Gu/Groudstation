9e31fb88a3b87e80976d83979a872df36e4bd40c
addischang1991 <addischang1991@livemail.tw>
2015-09-11 20:18:17 +0800

    Add GPS print out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 from math import pi
 
 # Innitialize the program
@@ -33,32 +33,47 @@ while gameLoop :
 		if (event.type == pygame.QUIT):
 			gameLoop = False
 	
-	#
 	window.fill( colorwhite )
+	window.blit( image, ( 0, 0 ) )
 	
-	#
-	aat = 100
-	labelaa1 = myfont.render( ( "Altitde: %s" % aat ) , 1, colorblack )
-	window.blit( labelaa1, ( 10, 420 ) )
+	for phase in range( 0, 314, 4 ):
 
-	sep = 10
-	labelaa2 = myfont.render( ( "  Speed: %s" % sep ), 1, colorblack )
-	window.blit( labelaa2, ( 10, 440 ) )
+		#
+		clock.tick(1)	
+	
+		#
+		window.fill( colorwhite )
+		window.blit( image, ( 0, 0 ) )
 
-	for XXX in range( 100, 300, 5 ):
+		#
+		aat = 100
+		labelaa1 = myfont.render( ( "Altitde: %s" % aat ) , 1, colorblack )
+		window.blit( labelaa1, ( 10, 420 ) )
 
-		clock.tick(1)
+		sep = 10
+		labelaa2 = myfont.render( ( "  Speed: %s" % sep ), 1, colorblack )
+		window.blit( labelaa2, ( 10, 440 ) )
 
-		window.blit( image, ( 0, 0 ) )		
-		
+		# Producing the fake GPS location
+		GPSX = round( 440 + 10 * math.cos( phase / ( 10 * pi ) ) )
+		GPSY = round( 220 - 10 * math.sin( phase / ( 10 * pi ) ) )
+
+		labelaa3 = myfont.render( ( "  GPS.X: %s" % GPSX ) , 1, colorblack )
+		window.blit( labelaa3, ( 10, 460 ) )
+
+		labelaa4 = myfont.render( ( "  GPS.Y: %s" % GPSY ) , 1, colorblack )
+		window.blit( labelaa4, ( 10, 480 ) )
+	
 		# Sketch the circle, but it should be the GPS posotion
-		pygame.draw.circle( window, colorred, ( 80+XXX, XXX ), 3, 3 )
+		pygame.draw.circle( window, colorred, ( GPSX, GPSY ), 3, 3 )
 		pygame.draw.circle( window, colorred, ( 700, 100 ), 50, 2 )
 		pygame.draw.circle( window, colorred, ( 700, 200 ), 50, 2 )
 	
 		# Sketch the arc. The definition of location for arc sketch 
 		# are ( cornor of x, cornor of y, length of x, length of y ) 
 		# pygame.draw.arc( window, colorgreen, ( 650, 50, 100, 100 ), 0, pi/2, 10 )
+
+		
 
 		# Go ahead and update the screen with what we've drawn.
 		# This MUST happen after all the other drawing commands.
